4fcac2304b0b2b1236c809d92a406fe79a822cff
KomeijiLogi <zclxuan2012@163.com>
2018-03-14 09:52:04 +0800

    修改差旅费报销单

--- BPMMobile/Pages/01WeGoHQ/03WeiHaiWeiDaSha/XX0103001/common.js
+++ BPMMobile/Pages/01WeGoHQ/03WeiHaiWeiDaSha/XX0103001/common.js
@@ -1,1 +1,221 @@
-﻿
+﻿function prepMsg() {
+    tapEvent();
+    $("#fdate").val(getNowFormatDate(2));
+    var xml = `<?xml version= "1.0" ?>
+               <Requests>
+               <Params>
+               <Method>GetFormPostData</Method>
+               <ProcessName>威海卫大厦西软信息维护申请</ProcessName>
+               <ProcessVersion>${version}</ProcessVersion>
+               <Owner></Owner>
+               </Params>
+               </Requests>
+    `;
+    dataProvider(xml, function (data) {
+        var provideData = JSON.parse(unescape(data.replace(/\\(u[0-9a-fA-F]{4})/gm, '%$1')));
+        console.log(provideData);
+        var item = provideData.Tables[0].Rows[0];
+        $("#fname").val(item.申请人);
+        $("#fdept").val(item.申请部门);
+    });
+
+}
+function dataProvider(xml, callback) {
+
+    var p = new Promise(function (resolve, reject) {
+        $.ajax({
+            type: "POST",
+            url: "/api/bpm/DataProvider",
+            data: { '': xml },
+            beforeSend: function (XHR) {
+                XHR.setRequestHeader('Authorization', 'Basic ' + localStorage.getItem('ticket'));
+            }
+        }).done(function (data) {
+            callback(data);
+            resolve();
+        }).fail(function (XMLHttpRequest, textStatus, errorThrown) {
+            if (XMLHttpRequest.status == "401") {
+                mui.alert('授权过期，请重新打开页面');;
+            } else if (XMLHttpRequest.status == "500") {
+                mui.alert('服务器内部错误');
+            }
+            reject();
+        });
+    });
+    return p;
+}
+function tapEvent() {
+
+}
+
+function initData(data, flag) {
+    var item = data.FormDataSet.威海卫大厦西软信息维护申请表[0];
+    if (flag) {
+        $("#taskId").val(item.TaskID);
+        $("#stepId").val(stepId);
+        $("#fbillno").val(item.fbillno);
+    }
+    $("#fname").val(item.申请人);
+    $("#fdept").val(item.申请部门);
+    $("#fdate").val(FormatterTimeYMS(item.申请日期));
+    $("#fsqnr").val(item.申请内容);
+    $("#fjdr").val(item.接单人);
+
+}
+
+function nodeControllerExp(NodeName) {
+    var item = data.FormDataSet.威海卫大厦西软信息维护申请表[0];
+    if (flag) {
+        $("#taskId").val(item.TaskID);
+        $("#stepId").val(stepId);
+        $("#fbillno").val(item.fbillno);
+    }
+    $("#fname").val(item.申请人);
+    $("#fdept").val(item.申请部门);
+    $("#fdate").val(FormatterTimeYMS(item.申请日期));
+    $("#fsqnr").val(item.申请内容);
+    $("#fjdr").val(item.接单人);
+    $("#fcnwcsj").val(FormatterTimeYMS(item.承诺完成时间));
+    $("#fclgc").val(item.处理过程);
+    $("#fclpj").val(item.处理评价);
+
+    if (item.附件 != null && item.附件 != "") {
+        var fjtmp = (String)(item.附件);
+
+        fjArray = fjtmp.split(";");
+
+
+        //console.log("fjArray:" + fjArray);
+
+        //请求附件详细信息
+        $.ajax({
+            type: 'POST',
+            url: '/api/bpm/GetAttachmentsInfo',
+            //dataType:'json',
+            data: { 'fileIds': fjArray },
+
+            beforeSend: function (XHR) {
+                XHR.setRequestHeader('Authorization', 'Basic ' + localStorage.getItem('ticket'));
+            },
+            success: function (data, status) {
+                if (status == "success") {
+
+                    console.log(data);
+
+                    for (var i = 0; i < data.length; i++) {
+
+                        var name = data[i].Name;
+                        var type = (data[i].Ext).replace(".", "");
+                        var size = String(data[i].Size);
+
+                        var time = String(data[i].LastUpdate).replace("T", " ");
+                        var downurl = baseDownloadUrl + data[i].FileID;
+
+                        var attach = attachItem(name, type, size, time, downurl);
+                        attachArray.push(attach);
+
+                        var li = '<div class="pic-preview smallyulan success">';
+                        li = li + ' <div class="del none" style="opacity:1;z-index:999;"onclick="delPicture(this)">x</div>';
+
+                        //类型判断 
+                        if ((data[i].Ext).indexOf("png") != -1 || (data[i].Ext).indexOf("jpg") != -1 || (data[i].Ext).indexOf("bmp") != -1) {
+
+                            //li = li + '    <div class="img-wrap smallimg" id="simg" ><a href="'+baseDownloadUrl + data[i].FileID + '"><img src="'+baseDownloadUrl + data[i].FileID + '"/></a></div>';
+                            li = li + '    <div class="img-wrap smallimg imgdiv" id="' + i + '" ><img src="' + baseDownloadUrl + data[i].FileID + '"/></div>';
+
+                        } else if ((data[i].Ext).indexOf("xls") != -1) {
+
+                            li = li + '    <div class="img-wrap smallimg imgdiv" id="' + i + '"><img src="../../Content/images/xlsx@2x.png"/></div>';
+
+                        } else if ((data[i].Ext).indexOf("doc") != -1) {
+
+                            li = li + '    <div class="img-wrap smallimg imgdiv" id="' + i + '"><img src="../../Content/images/docx@2x.png"/></div>';
+
+                        } else if ((data[i].Ext).indexOf("ppt") != -1) {
+
+                            li = li + '    <div class="img-wrap smallimg imgdiv" id="' + i + '"><img src="../../Content/images/ppt@2x.png"/></div>';
+
+                        } else if ((data[i].Ext).indexOf("pdf") != -1) {
+
+                            li = li + '    <div class="img-wrap smallimg imgdiv" id="' + i + '"><img src="../../Content/images/pdf@2x.png"/></div>';
+
+                        } else if ((data[i].Ext).indexOf("zip") != -1 || (data[i].Ext).indexOf("rar") != -1 || (data[i].Ext).indexOf("7z") != -1) {
+
+                            li = li + '    <div class="img-wrap smallimg imgdiv" id="' + i + '"><img src="../../Content/images/zip@2x.png"/></div>';
+
+                        } else if ((data[i].Ext).indexOf("txt") != -1) {
+
+                            li = li + '    <div class="img-wrap smallimg imgdiv" id="' + i + '"><img src="../../Content/images/txt@2x.png"/></div>';
+
+                        } else {
+                            li = li + '    <div class="img-wrap smallimg imgdiv" id="' + i + '"><img src="../../Content/images/unkown@2x.png"/></div>';
+                        }
+
+                        li = li + ' </div>';
+                        li = li + '</div>';
+                        $(".upload-img-list").append(li);
+
+
+                        $(".imgdiv").each(function () {
+
+                            $(this).parent().find(".del.none").hide();
+
+                        });
+
+
+
+
+                    }
+                    watch();
+                    $(".imgdiv").on('tap', function () {
+                        console.log($(this)[0].id);
+                        XuntongJSBridge.call('showFile', {
+                            'fileName': attachArray[$(this)[0].id].name,
+                            'fileExt': attachArray[$(this)[0].id].type,
+                            'fileTime': attachArray[$(this)[0].id].time,
+                            'fileSize': attachArray[$(this)[0].id].size,
+                            'fileDownloadUrl': attachArray[$(this)[0].id].downurl
+                        }, function (result) {
+                            //alert(JSON.stringify(result));
+                        });
+                    });
+
+
+                }
+
+            }, error: function (e) {
+                console.log(e);
+
+            }, complete: function () {
+
+            }
+
+        });
+
+    }
+
+}
+
+
+
+
+
+function Save() {
+    var fname = $("#fname").val();
+    var fdept = $("#fdept").val();
+    var fdate = $("#fdate").val();
+    var fsqnr = $("#fsqnr").val();
+
+
+
+}
+
+function reSave() {
+
+}
+function hasRead() {
+
+}
+function AgreeOrConSign() {
+
+}
