768cd1871e0124480ae8611d102b817707cdfa25
KomeijiLogi <zclxuan2012@163.com>
2018-05-16 17:00:07 +0800

    更新文件

---
+++ BPMMobile/Pages/02YiYongZhiPin/09JieLiKang/SC0209005/common.js
@@ -0,0 +1,58 @@
+﻿function prepMsg() {
+    $("#fdate").val(getNowFormatDate(2));
+    tapEvent();
+    var xml = `<?xml version= "1.0" ?>
+                  <Requests>
+                      <Params>
+                      <Method>GetFormPostData</Method>
+                      <ProcessName>洁丽康公司客户申请</ProcessName>
+                      <ProcessVersion>${version}</ProcessVersion>
+                      <Owner></Owner>
+                      </Params>   
+                  </Requests>
+              `;
+    $.ajax({
+        type: "POST",
+        url: "/api/bpm/DataProvider",
+        data: { '': xml },
+        beforeSend: function (XHR) {
+            XHR.setRequestHeader('Authorization', 'Basic ' + localStorage.getItem('ticket'));
+        }
+    }).done(function (data) {
+        var provideData = JSON.parse(unescape(data.replace(/\\(u[0-9a-fA-F]{4})/gm, '%$1')));
+        console.log(provideData);
+        var item = provideData.Tables[0].Rows[0];
+        
+
+    }).fail(function (e) {
+
+    });
+
+}
+
+function tapEvent() {
+
+
+}
+
+function initData(data, flag) {
+
+}
+
+function nodeControllerExp(NodeName) {
+
+    if (String(NodeName).match('开始') != null) {
+        tapEvent();
+    }
+
+}
+function checkNes() {
+
+    var NodeName = $("#nodeName").val();
+    return true;
+
+}
+
+function Save() {
+
+}
