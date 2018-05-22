cfb715fd5866d09e9e141cad68b7329b11ce35bb
KomeijiLogi <zclxuan2012@163.com>
2018-05-22 16:52:56 +0800

    更新流程

--- BPMMobile/Pages/00KCY/BG001606/common.js
+++ BPMMobile/Pages/00KCY/BG001606/common.js
@@ -108,8 +108,247 @@ function nodeControllerExp(NodeName) {
 
 
 class Mx {
-    constructor(fxm, fxb, fbanzu, fsfzh) {
+    constructor(fxm, fxb, fbanzu, fsfzh, flz_rq, flz_fs, fzsdd, fbz) {
 
+        this.fxm = fxm;
+        this.fxb = fxb;
+        this.fbanzu = fbanzu;
+        this.fsfzh = fsfzh;
+        this.flz_rq = flz_rq;
+        this.flz_fs = flz_fs;
+        this.fzsdd = fzsdd;
+        this.fbz = fbz;
+
+    }
+}
+
+function Save() {
+
+}
+
+function reSave() {
+
+}
+
+function hasRead() {
+    var pid = $("#stepId").val();
+    var fbillno = $("#fbillno").val();
+    var comment = '';
+    var btnArray = ['取消', '确定'];
+    mui.prompt('请选填知会意见', '可以不填', '知会意见', btnArray, function (e) {
+        if (e.index == 1) {
+            comment = e.value;
+            var xml = `<?xml version="1.0"?>
+                           <XForm>
+                             <Header>
+                               <Method>InformSubmit</Method>
+                               <PID>${pid}</PID>
+                               <Comment>${comment}</Comment>
+                             </Header>
+                           </XForm>
+              `;
+            PostXml(xml);
+        }
+    });
+}
+
+function AgreeOrConSign() {
+
+    var pid = $("#stepId").val();
+    var fbillno = $("#fbillno").val();
+    var comment = $("#signSuggest").val();
+    var nodeName = $("#nodeName").val();
+
+
+    var fname = $("#fname").val();
+    var fdate = $("#fdate").val();
+    var fssgs = $("#fssgs").val();
+    var fssfgs = $("#fssfgs").val();
+    var flxdh = $("#flxdh").val();
+
+    var mxlistArr = [];
+    var mxflag = false;
+    $("#mxlist").find("#mx").each(function () {
+
+        var fxm = $(this).find("#fxm").val();
+        var fxb = $(this).find("#fxb").val();
+        var fbanzu = $(this).find("#fbanzu").val();
+        var fsfzh = $(this).find("#fsfzh").val();
+        var flz_rq = $(this).find("#flz_rq").val();
+        var flz_fs = $(this).find("#flz_fs").val();
+        var fzsdd = $(this).find("#fzsdd").val();
+        var fbz = $(this).find("#fbz").val();
+
+        var mx = new Mx(fxm, fxb, fbanzu, fsfzh, flz_rq, flz_fs, fzsdd, fbz);
+        mxlistArr.push(mx);
+
+    });
+
+
+    var consignFlag = false;
+    var consignUserId = new Array();
+    var consignRoutingType;
+    var consignReturnType;
+
+    var consignUserStr;
+
+    //加签if分支
+    if (($('#signPer').val() != null) && ($('#signPer').val() != '')) {
+        consignFlag = true;
+
+        if ($('#sxsl').hasClass('mui-selected')) {
+            consignRoutingType = 'Serial';
+
+        } else if ($('#pxsl').hasClass('mui-selected')) {
+            consignRoutingType = 'Parallel';
+        }
+
+        if ($('#hdbjdl').hasClass('mui-selected')) {
+            consignReturnType = 'Return';
+        } else if ($('#jrxjdl').hasClass('mui-selected')) {
+            consignReturnType = 'Forward';
+        }
+
+
+        var consignAjax = $.ajax({
+            type: "POST",
+            url: "/api/bpm/PostAccount",
+            data: { "ids": consignOpenIdArr },
+            beforeSend: function (XHR) {
+                XHR.setRequestHeader('Authorization', 'Basic ' + localStorage.getItem('ticket'));
+
+            }
+        }).done(function (data, status) {
+            //alert(status);
+            if (status == "success") {
+
+
+                for (var i = 0; i < data.data.length; i++) {
+                    consignUserId.push(data.data[i].phone);
+                }
+                $('#consignUser').val(consignUserId);
+                consignUserStr = (String)($('#consignUser').val()).split(",");
+
+                for (var i = 0; i < consignUserStr.length; i++) {
+                    consignUserStr[i] = '&quot;' + consignUserStr[i] + '&quot;';
+                }
+                consignUserStr = '[' + consignUserStr.toString() + ']';
+
+
+
+            }
+        }).fail(function () {
+
+        });
+    } else {
+
+
+    }
+    if (consignFlag) {
+        consignAjax.then(function () {
+            var xml = `<?xml version="1.0"?>
+                     <XForm>
+                     <Header>
+                     <Method>Process</Method>
+                     <PID>${pid}</PID>
+                     <Action>同意</Action>
+                     <Comment>${comment}</Comment>
+            
+                     <ConsignEnabled>true</ConsignEnabled>
+                     <ConsignUsers>${consignUserStr}</ConsignUsers>
+                     <ConsignRoutingType>${consignRoutingType}</ConsignRoutingType>
+                     <ConsignReturnType>${consignReturnType}</ConsignReturnType>
+                     <InviteIndicateUsers>[]</InviteIndicateUsers>
+                     <Context>{&quot;Routing&quot;:{}}</Context>
+                     </Header>
+                     <FormData>`;
+            xml += `
+                      <BPM_WGJTYGLISSQLTB_A>
+                        <fbillno>${fbillno}</fbillno>
+                        <fname>${fname}</fname>
+                        <fdate>${fdate}</fdate>
+                        <fssgs>${fssgs}</fssgs>
+                        <ffgs>${fssfgs}</ffgs>
+                        <ftel>${flxdh}</ftel>
+                        <fj>201609230087</fj>
+                    </BPM_WGJTYGLISSQLTB_A>
+                    `;
+            for (var i = 0; i < mxlistArr.length; i++) {
+                xml += `
+                          <BPM_WGJTYGLISSQLTB_B>
+                            <RelationRowGuid>${i + 1}</RelationRowGuid>
+                            <RowPrimaryKeys>itemid=${itemidArr[i]}</RowPrimaryKeys>
+                            <fentyrno>${i + 1}</fentyrno>
+                            <fxm>${mxlistArr[i].fxm}</fxm>
+                            <fxb>${mxlistArr[i].fxb}</fxb>
+                            <fbanzu>${mxlistArr[i].fbanzu}</fbanzu>
+                            <fsfzh>${mxlistArr[i].fsfzh}</fsfzh>
+                            <flz_rq>${mxlistArr[i].flz_rq}</flz_rq>
+                            <flz_fs>${mxlistArr[i].flz_fs}</flz_fs>
+                            <fzsdd>${mxlistArr[i].fzsdd}</fzsdd>
+                            <fbz>${mxlistArr[i].fbz}</fbz>
+                        </BPM_WGJTYGLISSQLTB_B>
+                        `;
+            }
+            xml += `
+                       </FormData>
+                    </XForm>
+                   `;
+            PostXml(xml);
+        })
+    } else {
+
+        var xml = `<?xml version="1.0"?>
+                   <XForm>
+                   <Header>
+                   <Method>Process</Method>
+                   <PID>${pid}</PID>
+                   <Action>同意</Action>
+                   <Comment>${comment}</Comment>
+
+                    <UrlParams></UrlParams>
+                    <ConsignEnabled>false</ConsignEnabled>
+                    <ConsignUsers>[]</ConsignUsers>
+                    <ConsignRoutingType>Parallel</ConsignRoutingType>
+                    <ConsignReturnType>Return</ConsignReturnType>
+
+                  <InviteIndicateUsers>[]</InviteIndicateUsers>
+                  <Context>{&quot;Routing&quot;:{}}</Context>
+                  </Header>
+                  <FormData>`;
+        xml += `
+                      <BPM_WGJTYGLISSQLTB_A>
+                        <fbillno>${fbillno}</fbillno>
+                        <fname>${fname}</fname>
+                        <fdate>${fdate}</fdate>
+                        <fssgs>${fssgs}</fssgs>
+                        <ffgs>${fssfgs}</ffgs>
+                        <ftel>${flxdh}</ftel>
+                        <fj>201609230087</fj>
+                    </BPM_WGJTYGLISSQLTB_A>
+                    `;
+        for (var i = 0; i < mxlistArr.length; i++) {
+            xml += `
+                          <BPM_WGJTYGLISSQLTB_B>
+                            <RelationRowGuid>${i + 1}</RelationRowGuid>
+                            <RowPrimaryKeys>itemid=${itemidArr[i]}</RowPrimaryKeys>
+                            <fentyrno>${i + 1}</fentyrno>
+                            <fxm>${mxlistArr[i].fxm}</fxm>
+                            <fxb>${mxlistArr[i].fxb}</fxb>
+                            <fbanzu>${mxlistArr[i].fbanzu}</fbanzu>
+                            <fsfzh>${mxlistArr[i].fsfzh}</fsfzh>
+                            <flz_rq>${mxlistArr[i].flz_rq}</flz_rq>
+                            <flz_fs>${mxlistArr[i].flz_fs}</flz_fs>
+                            <fzsdd>${mxlistArr[i].fzsdd}</fzsdd>
+                            <fbz>${mxlistArr[i].fbz}</fbz>
+                        </BPM_WGJTYGLISSQLTB_B>
+                        `;
+        }
+        xml += `
+                       </FormData>
+                    </XForm>
+                   `;
+        PostXml(xml);
 
     }
 }
--- BPMMobile/Pages/00KCY/BG001608/common.js
+++ BPMMobile/Pages/00KCY/BG001608/common.js
@@ -1,1 +1,174 @@
-﻿
+﻿function prepMsg() {
+    $("#fdate").val(getNowFormatDate(2));
+    tapEvent();
+    uploadOpt();
+    var xml = `<?xml version= "1.0" ?>
+                  <Requests>
+                      <Params>
+                      <Method>GetFormPostData</Method>
+                      <ProcessName>集团公司员工车辆通行证申请</ProcessName>
+                      <ProcessVersion>${version}</ProcessVersion>
+                      <Owner></Owner>
+                      </Params>   
+                  </Requests>
+              `;
+    $.ajax({
+        type: "POST",
+        url: "/api/bpm/DataProvider",
+        data: { '': xml },
+        beforeSend: function (XHR) {
+            XHR.setRequestHeader('Authorization', 'Basic ' + localStorage.getItem('ticket'));
+        }
+    }).done(function (data) {
+        var provideData = JSON.parse(unescape(data.replace(/\\(u[0-9a-fA-F]{4})/gm, '%$1')));
+        console.log(provideData);
+        var item = provideData.Tables[0].Rows[0];
+        $("#fname").val(item.fname);
+        $("#fgs").val(item.fgs);
+
+
+    }).fail(function (e) {
+
+    });
+}
+
+
+function tapEvent() {
+
+    var flxdata = [
+        {
+            value: '',
+            text:'公车'
+        },
+        {
+            value: '',
+            text:'私车'
+        }
+    ];
+    var element = document.getElementById('flx');
+
+    var picker = new mui.PopPicker();
+
+    picker.setData(flxdata);
+
+    element.addEventListener('tap', function () {
+
+        picker.show(function (items) {
+
+            element.value = items[0].text;
+            if (items[0].text == '公车') {
+
+            } else {
+
+            }
+        });
+
+    }, false);
+
+
+    $("#tjmx").on('tap', function () {
+
+        var li = `
+                   <div id="mx" class="mui-card">
+                      <div class="mui-input-row itemtitle">
+                         <label>明细列表项</label>
+                         <span class="mui-icon mui-icon-close mui-pull-right" style="margin-right:0.6rem;border-width:0.1rem;border-radius:1.2rem;margin-top:0.2rem;" id="deleteProduct" onclick="deleteItem(this)"></span>
+                      </div>
+                      <div class="mui-row cutOffLine" style="padding:3vw;">
+                         <div class="mui-col-xs-4" style="display:flex;">
+                            <label>姓名<i style="color:red;">*</i></label>
+                            <textarea rows="2" id="fxm" readonly placeholder="请选择"></textarea> 
+                         </div>
+                          <div class="mui-col-xs-4" style="display:flex;">
+                             <label>所属集团</label>
+                             <textarea rows="2" id="fss_jt" readonly></textarea>
+                         </div>
+                         <div class="mui-col-xs-4" style="display:flex;">
+                             <label>所属公司</label> 
+                             <textarea rows="2" id="fss_gs" readonly></textarea>
+                         </div>                           
+                      </div>
+                       <div class="mui-row cutOffLine" style="padding:3vw;">
+                            <div class="mui-col-xs-4" style="display:flex;">
+                                <label>手机号码<i style="color:red;">*</i></label>
+                                <textarea rows="2" id="fsjh" readonly></textarea> 
+                            </div>
+                            <div class="mui-col-xs-4" style="display:flex;">
+                                <label>车牌号<i style="color:red;">*</i></label>
+                                <textarea rows="2" id="fcph" readonly></textarea>  
+                            </div>
+                            <div class="mui-col-xs-4" style="display:flex;">
+                                <label>车型<i style="color:red;">*</i></label>
+                                <textarea rows="2" id="fcx" readonly></textarea> 
+                            </div>
+                       </div>
+                        <div class="mui-input-row cutOffLine" style="height:7rem;overflow:scroll;" id="uploaddiv">
+                                <div class="border border-t upload-img" style="top:0rem;">
+                                    <div class="clearfix upload-btn" id="children-bg">
+                                        <label class="label">行驶证<i style="color:red;">*</i></label>
+                                        <span class="tips" id="imageCount"></span>
+                                        <span class="upload-addbtn">
+                                            <input type="file" accept="image/jpeg,image/jpg,image/png,image/jp2,image/bmp" id="file" style="opacity:0;">
+                                        </span>
+                                    </div>
+                                    <div class="upload-img-list"></div>
+                                </div>
+                        </div>
+                      <div class="mui-row cutOffLine" style="padding:3vw;">
+                          <div class="mui-col-xs-12" style="display:flex;">
+                              <label>行驶证持有人<i style="color:red;">*</i></label> 
+                              <textarea rows="2" id="fcyr" readonly ></textarea>
+                          </div>
+                      </div>
+                        <div class="mui-input-row cutOffLine" style="height:7rem;overflow:scroll;" id="uploaddiv">
+                                <div class="border border-t upload-img" style="top:0rem;">
+                                    <div class="clearfix upload-btn" id="children-bg">
+                                        <label class="label">驾驶证<i style="color:red;">*</i></label>
+                                        <span class="tips" id="imageCount"></span>
+                                        <span class="upload-addbtn">
+                                            <input type="file" accept="image/jpeg,image/jpg,image/png,image/jp2,image/bmp" id="file" style="opacity:0;">
+                                        </span>
+                                    </div>
+                                    <div class="upload-img-list"></div>
+                                </div>
+                        </div>
+                        <div class="mui-input-row cutOffLine" style="height:7rem;overflow:scroll;" id="uploaddiv">
+                                <div class="border border-t upload-img" style="top:0rem;">
+                                    <div class="clearfix upload-btn" id="children-bg">
+                                        <label class="label">结婚证<i style="color:red;">*</i></label>
+                                        <span class="tips" id="imageCount"></span>
+                                        <span class="upload-addbtn">
+                                            <input type="file" accept="image/jpeg,image/jpg,image/png,image/jp2,image/bmp" id="file" style="opacity:0;">
+                                        </span>
+                                    </div>
+                                    <div class="upload-img-list"></div>
+                                </div>
+                        </div>
+                        <div class="mui-row cutOffLine" style="padding:3vw;">
+                            <div class="mui-col-xs-12" style="display:flex;">
+                                 <label>车辆通行证号<i style="color:red;">*</i></label>    
+                                 <textarea rows="2" id="ftxzh" readonly></textarea>
+                            </div>     
+                        </div> 
+                  </div>
+                  `;
+        $("#mxlist").append(li);
+    });
+
+
+}
+
+
+function initData(data, flag) {
+    var item = data.FormDataSet.BPM_YGCLTXZSQ_A[0];
+    if (flag) {
+        $("#taskId").val(item.TaskID);
+        $("#stepId").val(stepId);
+        $("#fbillno").val(item.fbillno);
+    }
+    $("#fname").val(item.fname);
+    $("#fdate").val(FormatterTimeYMS(item.fdate));
+    $("#fgs").val(item.fgs);
+
+
+}
